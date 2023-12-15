15fc358b795eb3d1e04fe1a5ce8159ca1fa96068
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-15 18:03:41 -0500

    cors file changed to general for testing

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const storeRoute = require("./routes/stores");
 const inventoriesRoute = require("./routes/inventories");
 
 const app = express();
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+// app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 app.use(express.json());
 app.use("/api/stores", storeRoute);
 app.use("/api/inventories", inventoriesRoute);
